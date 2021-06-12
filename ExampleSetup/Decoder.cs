2f20ea5899601aa9d065f4856cbaa49c4860981b
rytis <rytisp@gmail.com>
2021-06-12 21:59:02 +0100

    More Clean Up Unused References

--- ExampleSetup/Decoder.cs
+++ ExampleSetup/Decoder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ExampleSetup
+﻿namespace ExampleSetup
 {
     public static class Decoder
     {
